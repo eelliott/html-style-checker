64b3af3c4f8e1bce76754131c1b153814c3cd564
eelliott <elijah.elliott96@gmail.com>
2016-06-01 13:09:01 -0700

    Added functionality for validating the line length for HTML documents when command is run.

--- src/extension.ts
+++ src/extension.ts
@@ -11,19 +11,52 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "html-style-checker" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    let lineChecker = new LineChecker();
+    
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        lineChecker.checkLines();
     });
-
+    
+    context.subscriptions.push(lineChecker);
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class LineChecker {
+    private longLines = vscode.languages.createDiagnosticCollection("longLines");
+    
+    public checkLines() {
+        let diagnostics: vscode.Diagnostic[] = [];
+        let lineCount: number = 0;
+        
+        let editor = vscode.window.activeTextEditor;
+        // if there is no editor instance it will just return.
+        if (!editor || editor.document.languageId != "html") {
+            return;
+        }
+        
+        let doc = editor.document;
+        let docContent = doc.getText();
+        let lines = docContent.split("\n");
+        
+        for (var i = 0; i < lines.length; i++) {
+            if (lines[i].length >= 100) {
+                let startPos = new vscode.Position(i, doc.lineAt(i).firstNonWhitespaceCharacterIndex);
+                let endPos = new vscode.Position(i, lines[i].length);
+                let diagnostic = new vscode.Diagnostic(new vscode.Range(startPos, endPos),
+                    "Lines must be less than 100 characters", vscode.DiagnosticSeverity.Error);
+                diagnostics.push(diagnostic);
+                lineCount++;
+            }
+        }
+        this.longLines.set(editor.document.uri, diagnostics);
+    }
+    
+    public dispose() {
+        this.longLines.clear();
+        this.longLines.dispose();
+    }
 }
