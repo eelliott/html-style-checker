a9991ff4953b90c9386e89b61a761df3831dacb8
eelliott <elijah.elliott96@gmail.com>
2016-06-02 14:39:31 -0700

    began to add functionality for validating html tag style

--- src/extension.ts
+++ src/extension.ts
@@ -25,11 +25,12 @@ export function deactivate() {
 
 class LineChecker {
     private longLines = vscode.languages.createDiagnosticCollection("longLines");
+    private tags = vscode.languages.createDiagnosticCollection("tags");
     
     public checkLines() {
-        let diagnostics: vscode.Diagnostic[] = [];
-        let lineCount: number = 0;
-        
+        let lineDiagnostics: vscode.Diagnostic[] = [];
+        let tagDiagnostics: vscode.Diagnostic[] = [];
+                
         let editor = vscode.window.activeTextEditor;
         // if there is no editor instance it will just return.
         if (!editor) {
@@ -37,22 +38,29 @@ class LineChecker {
         }
         
         let doc = editor.document;
+        
         if (doc.languageId === "html") {
             let docContent = doc.getText();
             let lines = docContent.split("\n");
             
             for (var i = 0; i < lines.length; i++) {
+                this.validateTag(lines[i]);
                 if (lines[i].length >= 100) {
                     let startPos = new vscode.Position(i, doc.lineAt(i).firstNonWhitespaceCharacterIndex);
                     let endPos = new vscode.Position(i, lines[i].length);
                     let diagnostic = new vscode.Diagnostic(new vscode.Range(startPos, endPos),
                         "Lines must be less than 100 characters", vscode.DiagnosticSeverity.Error);
-                    diagnostics.push(diagnostic);
-                    lineCount++;
+                    lineDiagnostics.push(diagnostic);
                 }
             }
-            this.longLines.set(editor.document.uri, diagnostics);
+            this.longLines.set(editor.document.uri, lineDiagnostics);
         }
+    }
+    
+    public validateTag(line: string) {
+        let jsonURI:vscode.Uri = vscode.Uri.file(vscode.workspace.rootPath + "/src/html-rules/tags.json");
+        let tags = jsonURI.toJSON();
+        console.log(tags.selfClosing.tags);
     }
     
     public dispose() {
